8ea4a2d4b22969940f3828c9ffe7767aa3ed8651
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-09 21:17:38 +0200

    Return constructor injection

--- src/main/kotlin/com/iurii/springcoredemo/rest/DemoController.kt
+++ src/main/kotlin/com/iurii/springcoredemo/rest/DemoController.kt
@@ -1,19 +1,15 @@
 package com.iurii.springcoredemo.rest
 
 import com.iurii.springcoredemo.common.Coach
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class DemoController {
-
-    @set:Autowired
-    var coach: Coach? = null
+class DemoController(private val coach: Coach) {
 
     @GetMapping("/dailyworkout")
     fun dailyWorkout(): ResponseEntity<String> {
-        return ResponseEntity.ok(coach?.getDailyWorkout())
+        return ResponseEntity.ok(coach.getDailyWorkout())
     }
 }
