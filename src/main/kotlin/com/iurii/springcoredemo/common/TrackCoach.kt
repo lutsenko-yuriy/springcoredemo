607fe2928e8dffff97a569a3218c010aba4364b1
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-09 22:38:08 +0200

    Add identity check

--- src/main/kotlin/com/iurii/springcoredemo/common/TrackCoach.kt
+++ src/main/kotlin/com/iurii/springcoredemo/common/TrackCoach.kt
@@ -1,8 +1,11 @@
 package com.iurii.springcoredemo.common
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 class TrackCoach : Coach {
 
     init {
--- src/main/kotlin/com/iurii/springcoredemo/rest/DemoController.kt
+++ src/main/kotlin/com/iurii/springcoredemo/rest/DemoController.kt
@@ -7,10 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class DemoController(@Qualifier("trackCoach") private val coach: Coach) {
+class DemoController(
+    @Qualifier("trackCoach") private val coach: Coach,
+    @Qualifier("trackCoach") private val anotherCoach: Coach
+) {
 
     @GetMapping("/dailyworkout")
     fun dailyWorkout(): ResponseEntity<String> {
         return ResponseEntity.ok(coach.getDailyWorkout())
     }
+
+    @GetMapping("/identity")
+    fun identity(): ResponseEntity<Boolean> {
+        return ResponseEntity.ok(coach === anotherCoach)
+    }
+
 }
